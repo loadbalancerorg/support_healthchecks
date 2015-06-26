3dfb0e31043f782f7f06ccc14f659cae87057c5c
Andrew Samalley <asmalley@loadbalancer.org>
2015-06-26 16:38:12 +0100

    removed echo $? exit code from printing

--- radiushealthcheck.sh
+++ radiushealthcheck.sh
@@ -2,8 +2,9 @@
 username=steve
 password=testing
 nashost=$2
+#nasport=$3 # does not work with firewallmarks - must manually set port below if multiport VIP
 nasport=1812
 nassecret=testing123
 nasname=localhost
 radtest $username $password $nashost:$nasport 0 $nassecret 0 $nasname >/dev/null
-echo $?
+#echo $?
