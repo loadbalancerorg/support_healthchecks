bc5b038d7d49c9964fb7f71fb5812d3df830ecbc
Neil Stone <neil.stone@loadbalancer.org>
2018-08-17 16:27:38 +0100

    http-status-check.sh - Initial commit

---
+++ http-status-check.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#set -x
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
+
+########################################################################
+#
+# Script to check for a defined http status and pass if it's presented.
+#
+# Neil Stone - support@loadbalancer.org
+#
+# v 1.0 initial commit
+#
+########################################################################
+
+DESIRED_STATUS=401
+
+#$1 = VIP Address
+#$2 = VIP Port
+#$3 = Real Server IP
+#$4 = Real Server Port
+
+if [ ${#} -lt 3 ]; then
+    exit 12
+fi
+
+#Script Variables for check port.
+CHECK_IP="${3}"
+
+if [ ! -z "${4}" ]; then
+    CHECK_PORT="${4}"
+else
+    CHECK_PORT="${2}"
+fi
+
+### Set the absolute path here...
+CHECK_PATH="authpage/index.html" # Path to fetch
+
+
+# first for https, second for http.
+# might also want to add host header as per:   -H 'Host: myhostname.mydomain.com' in the curl command(s)
+#
+STATUS=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' -k https://${CHECK_IP}:${CHECK_PORT}/${CHECK_PATH})
+#STATUS=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://${CHECK_IP}:${CHECK_PORT}/${CHECK_PATH})
+
+if [ ${STATUS} -ne ${DESIRED_STATUS} ] ; then
+# No match = exit with error
+    exit 1
+else
+# Match = exit clean
+	exit 0
+fi
