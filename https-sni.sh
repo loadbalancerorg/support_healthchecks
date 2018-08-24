f7a84af2c8904645251a46989447741cfc71b71b
Neil Stone <neil.stone@loadbalancer.org>
2018-08-24 12:43:28 +0100

    https-sni.sh - Hopefully a final version, with customer's details removed

--- https-sni.sh
+++ https-sni.sh
@@ -7,7 +7,7 @@
 #
 # (c) Loadbalancer.org 2018
 #
-# Neil Stone <support@loadbalancer.org>
+# 2018-08-24 - Initial write - Neil Stone <support@loadbalancer.org>
 #
 #############################################################
 
@@ -17,9 +17,10 @@
 #RIP=${3}
 #RPT=${4}
 
-CHECK_HOST="gdpr.marshalls.co.uk"
-CHECK_PATH="LoadbalancerStatus/LoadBalancer-Status-Page.aspx"
-CHECK_STRING="Loadbalancer Status Page"
+CHECK_HOST="https.site.address"
+CHECK_PATH="LoadbalancerStatus.php"
+CHECK_STRING="Success"
+
 
 ### Nothing below here should need changing...
 
@@ -31,17 +32,15 @@ fi
 # Script Variables for check port.
 CHECK_IP="${3}"
 
-if [ "${4}" -ne "" ]; then
-    CHECK_PORT="${4}"
-else
+if [ -z "${4}" ]; then
     CHECK_PORT="${2}"
+else
+    CHECK_PORT="${4}"
 fi
 
 CURL_OPTS="--resolve ${CHECK_HOST}:${CHECK_PORT}:${CHECK_IP}"
 
-# curl --resolve gdpr.marshalls.co.uk:443:10.101.147.15 -m 2 -H 'Host: gdpr.marshalls.co.uk' -i -k https://gdpr.marshalls.co.uk/LoadbalancerStatus/LoadBalancer-Status-Page.aspx
-
-curl ${CURL_OPTS} -H 'Host: ${CHECK_HOST}' -m 2 -i -k https://${CHECK_HOST}/${CHECK_PATH} | grep -q ${CHECK_STRING}
+curl ${CURL_OPTS} -H 'Host: '${CHECK_HOST}'' -m 2 -i -k https://${CHECK_HOST}/${CHECK_PATH} 2>/dev/null | grep -q "${CHECK_STRING}"
 
 EXIT_STATE=${?}
 
